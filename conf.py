0681e80aa0e6e9ffafaf3741f0aff24e5a71cfbe
FloreKA03 <raissa.tiomela@kaeyros-analytics.de>
2025-11-27 17:59:06 +0100

    first version of documentation cocobeach

---
+++ conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Documentation COCOBEACH'
+copyright = '2025, Kaeyros Analytics SARL'
+author = 'Kaeyros Analytics SARL'
+release = '2025'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",    # permet Google/NumPy style docstrings
+    "sphinx.ext.viewcode"     # ajoute un bouton Voir le code
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'fr'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ['_static']
